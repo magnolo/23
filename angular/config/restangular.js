49ee8813599a2be38a6f205bebabbfae9bf30fe5
Manfred Walder <office@manfredwalder.at>
2016-03-04 17:51:23 +0100

    enable api caching

--- angular/config/restangular.js
+++ angular/config/restangular.js
@@ -1,23 +1,27 @@
-(function(){
+(function() {
 	"use strict";
 
-	angular.module('app.config').config( function(RestangularProvider) {
+	angular.module('app.config').config(function(RestangularProvider) {
 		RestangularProvider
-		.setBaseUrl('/api/')
-		.setDefaultHeaders({ accept: "application/x.laravel.v1+json" })
-		.setDefaultHttpFields({cache: false})
-		.addResponseInterceptor(function(data,operation,what,url,response,deferred) {
-        var extractedData;
-        extractedData = data.data;
-        if (data.meta) {
-            extractedData._meta = data.meta;
-        }
-        if (data.included) {
-            extractedData._included = data.included;
-        }
-        return extractedData;
-    });
-	/*	.setErrorInterceptor(function(response, deferred, responseHandler) {
+			.setBaseUrl('/api/')
+			.setDefaultHeaders({
+				accept: "application/x.laravel.v1+json"
+			})
+			.setDefaultHttpFields({
+				cache: true
+			})
+			.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+				var extractedData;
+				extractedData = data.data;
+				if (data.meta) {
+					extractedData._meta = data.meta;
+				}
+				if (data.included) {
+					extractedData._included = data.included;
+				}
+				return extractedData;
+			});
+		/*	.setErrorInterceptor(function(response, deferred, responseHandler) {
 			console.log('errro');
 			if (response.status === 403) {
 
