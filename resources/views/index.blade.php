13ac8c535aec874f0c29080b331fe4a534dbfc86
Manfred Walder <office@manfredwalder.at>
2016-03-04 20:31:33 +0100

    favicons... icons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,27 @@
       <link href='//fonts.googleapis.com/css?family=Roboto:500,400' rel='stylesheet' type='text/css'>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <title update-title>23° | mapping data for global understanding</title>
-
-
       <meta name="apple-mobile-web-app-capable" content="yes">
       <meta name="apple-mobile-web-app-status-bar-style" content="black">
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+      <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+      <meta name="msapplication-square70x70logo" content="/smalltile.png" />
+      <meta name="msapplication-square150x150logo" content="/mediumtile.png" />
+      <meta name="msapplication-wide310x150logo" content="/widetile.png" />
+      <meta name="msapplication-square310x310logo" content="/largetile.png" />
+
       <!--[if lte IE 10]>
       <script type="text/javascript">document.location.href = '/unsupported-browser'</script>
       <![endif]-->
