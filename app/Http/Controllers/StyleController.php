581dc412fc53649ab116d53e3ea902d7385e5b30
Manfred Walder <office@manfredwalder.at>
2016-06-02 16:46:53 +0200

    extending style functionallity

--- app/Http/Controllers/StyleController.php
+++ app/Http/Controllers/StyleController.php
@@ -45,6 +45,17 @@ class StyleController extends Controller
         $style->title = $request->input('title');
         $style->name = str_slug($request->input('title'));
         $style->base_color = $request->input('base_color');
+        $style->basemap_id = $request->input('basemap_id');
+        $style->fixed_title = $request->input('fixed_title');
+        $style->fixed_description = $request->input('fixed_description');
+        $style->search_box = $request->input('search_box');
+        $style->share_options = $request->input('share_options');
+        $style->zoom_controls = $request->input('zoom_controls');
+        $style->scroll_wheel_zoom = $request->input('scroll_wheel_zoom');
+        $style->layer_selection = $request->input('layer_selection');
+        $style->legends = $request->input('legends');
+        $style->full_screen = $request->input('full_screen');
+
         $status = $style->save();
 
         return response()->api($style);
