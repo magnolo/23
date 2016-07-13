f325d47a98b645637b385a5a24203a8fd08e8e11
aldorado <anuar.suarez@hotmail.com>
2016-07-13 13:28:35 +0200

    Added documentation to DataService

--- angular/services/data.js
+++ angular/services/data.js
@@ -1,3 +1,14 @@
+/**
+ * @ngdoc service
+ * @name app.DataService
+ * @requires Restangular
+ * @requires toastr
+ *
+ * @description
+ * Provides CRUD services for API
+ *
+ */
+
 (function(){
     "use strict";
 
@@ -14,6 +25,18 @@
           remove: remove
         };
 
+        /**
+         * @ngdoc method
+         * @name app.DataService#getAll
+         * @methodOf app.DataService
+         *
+         * @description
+         * Gets all objects on given route filtered by filter
+         *
+         * @param {string} route address from which data will be pulled from
+         * @param {object} filter filter by which the pulled data will be filtered
+         * @returns {object} data json object with pulled data
+         */
         function getAll(route, filter){
           var data = Restangular.all(route).getList(filter);
             data.then(function(){}, function(data){
@@ -21,9 +44,35 @@
             });
             return data;
         }
+
+        /**
+         * @ngdoc method
+         * @name app.DataService#getOne
+         * @methodOf app.DataService
+         *
+         * @description
+         * Get on object on given route specified by given id
+         *
+         * @param {string} route address from which data will be pulled from
+         * @param {string} id datum id
+         * @returns {object} data json with pulled data
+         */
         function getOne(route, id){
           return Restangular.one(route, id).get();
         }
+
+        /**
+         * @ngdoc method
+         * @name app.DataService#post
+         * @methodOf app.DataService
+         *
+         * @description
+         * Create multiple new data entries
+         *
+         * @param {string} route route where to post to
+         * @param {object} data object to be created
+         * @returns {object} saved data
+         */
         function post(route, data){
           var data = Restangular.all(route).post(data);
           data.then(function(){}, function(data){
@@ -31,12 +80,50 @@
           });
           return data;
         }
+
+        /**
+         * @ngdoc method
+         * @name app.DataService#put
+         * @methodOf app.DataService
+         *
+         * @description
+         * Update or create multiple data entries
+         *
+         * @param {string} route route where to put at
+         * @param {object} data data object to be created or updated data object
+         */
         function put(route, data){
           return Restangular.all(route).put(data);
         }
+
+        /**
+         * @ngdoc method
+         * @name app.DataService#update
+         * @methodOf app.DataService
+         *
+         * @description
+         * Update a single data entry
+         *
+         * @param {string} route route to where to update at
+         * @param {string} id datum id
+         * @param {object} data updated datum
+         *
+         */
         function update(route, id, data){
           return Restangular.one(route, id).put(data);
         }
+
+        /**
+         * @ngdoc method
+         * @name app.DataService#remove
+         * @methodOf app.DataService
+         *
+         * @description
+         * Remove a single data entry
+         *
+         * @param {string} route where to delete at
+         * @param {string} id datum id
+         */
         function remove(route, id){
           return Restangular.one(route, id).remove();
         }
